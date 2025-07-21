db29d7500b087b22903e45e52c38b05141d5ce2c
ParthKhiriya <b23ee1051@iitj.ac.in>
2025-07-21 22:43:40 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className='text-3xl'>Welcome to My 3D Portfolio</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
